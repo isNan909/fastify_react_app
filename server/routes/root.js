408047822d50dee4d546579be85c55d6904dc222
Ishan Manandhar <ishan02016@gmail.com>
2021-05-30 19:04:42 +0545

    basic plugin

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (req, res) {
-    res.send({ message: 'We are running fastify here!' });
+// curl localhost:5000/api/lang
+module.exports = async function (fastify, _opts) {
+  const lang = fastify.lang();
+  fastify.get('/api/lang', async function (_req, res) {
+    res.send(lang);
   });
 };
