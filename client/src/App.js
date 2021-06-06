07f7acf89b157c2fe3bacfc8782bc1ad5bcf10b3
Ishan Manandhar <ishan02016@gmail.com>
2021-06-06 17:48:20 +0545

    added wip for react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="text-[3em] text-[#53ce0c] font-bold">
+          Hello 👋, tailwind jit React!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
